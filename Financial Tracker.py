52e6799ddbc22e638114e8c5e084ee798da2cfd0
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-03-03 11:17:34 +0100

    Initial commit

---
+++ Financial Tracker.py
@@ -0,0 +1,4 @@
+#Our idea for this final project was to create a useful project that can be used by everyone.
+#Because of this, we decided to do a financial tracker, which allows you to input daily spending and
+#see where you're at and the spending you've done this month.
+
